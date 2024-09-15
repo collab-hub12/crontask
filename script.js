aa8bb4412df07ccdef26b1b8b7c244f8f9429752
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-09-15 21:35:21 +0530

    fix: amqp connection

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const { createClient } = require("@libsql/client");
 const { getTaskDeadlineStatus } = require("./util.js");
+const amqp = require("amqplib");
 
 const main = async () => {
   //connect with db client
   const turso = createClient({
     url: process.env.TURSO_DATABASE_URL,
     authToken: process.env.TURSO_AUTH_TOKEN,
+  });
+
+  const mq_connection = await amqp.connect(process.env.MQ_CONNECTION_URL);
+  const channel = await mq_connection.createChannel();
+
+  const queue = process.env.QUEUE;
+
+  await channel.assertQueue(queue, {
+    durable: true,
   });
 
   const today = new Date();
@@ -26,21 +36,45 @@ const main = async () => {
   const tasksWithUsers = result.rows;
 
   // insert notification details concurrently
-  Promise.all(
-    tasksWithUsers.map((taskwithUser) => {
-      return turso.execute(
-        ` insert into notifications (user_id , task_id, notifed_at, description)
-          values (?,?,?,?)
+  await Promise.all(
+    tasksWithUsers.map(async (taskwithUser) => {
+      try {
+        const response = await turso.execute(
+          ` insert into notifications (user_id , task_id, notifed_at, description)
+          values (?,?,?,?) returning *
           `,
-        [
-          taskwithUser.user_id,
-          taskwithUser.task_id,
-          today.toISOString(),
-          getTaskDeadlineStatus(taskwithUser.title, taskwithUser.task_deadline),
-        ]
-      );
+          [
+            taskwithUser.user_id,
+            taskwithUser.task_id,
+            today.toISOString(),
+            getTaskDeadlineStatus(
+              taskwithUser.title,
+              taskwithUser.task_deadline
+            ),
+          ]
+        );
+        const [notification] = response.rows;
+
+        if (!notification) {
+          console.log(`there isnt any notification`);
+        }
+        //send message to the queue
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(notification)), {
+          persistent: true,
+        });
+        console.log(
+          `notification:(${notification.id})ID  has been sent to ${queue}`
+        );
+      } catch (err) {
+        console.log(err);
+        await channel.close();
+        await mq_connection.close();
+      }
     })
   );
+
+  await channel.close();
+  await mq_connection.close();
 };
 
 main();
